380b79608c666cc03f62f6465c89b8ce6c5d3457
HHFW <hanzhuguoying@163.com>
2021-05-27 14:43:17 +0800

    信号量限制线程执行顺序

--- springboot/demo/src/main/java/com/exm/demo/test/thread/domain/Foo.java
+++ springboot/demo/src/main/java/com/exm/demo/test/thread/domain/Foo.java
@@ -1,16 +1,24 @@
 package com.exm.demo.test.thread.domain;
 
+import java.util.concurrent.Semaphore;
+
 public class Foo {
+    private Semaphore first = new Semaphore(0);
+    private Semaphore second = new Semaphore(0);
 
-    public void first(Runnable printFirst) {
-        printFirst.run();
+    public void first() throws InterruptedException {
+        System.out.println("first");
+        first.release();
     }
 
-    public void second(Runnable printSecond) {
-        printSecond.run();
+    public void second() throws InterruptedException {
+        first.acquire();
+        System.out.println("second");
+        second.release();
     }
 
-    public void third(Runnable printThird) {
-        printThird.run();
+    public void third() throws InterruptedException {
+        second.acquire();
+        System.out.println("thread");
     }
 }
